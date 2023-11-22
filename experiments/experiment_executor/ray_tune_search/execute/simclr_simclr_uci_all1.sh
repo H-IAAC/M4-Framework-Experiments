289385ddb1e508aa2904f8f8fdc45b0158f6c20f
Amparo Diaz Muñoz <amparo.munoz.eldorado@gmail.com>
2023-11-22 17:20:47 -0300

    add experiments

--- experiments/experiment_executor/ray_tune_search/execute/simclr_simclr_uci_all1.sh
+++
@@ -1,5 +0,0 @@
-python hsearch_main.py --data ../data/ --cpu 0.2 --gpu 0.2 --max_concurrent 20 --experiment simclr_all/all_transformations/simclr/transf/uci/simclr_uci_P100 & 
-python hsearch_main.py --data ../data/ --cpu 0.2 --gpu 0.2 --max_concurrent 20 --experiment simclr_all/all_transformations/simclr/transf/uci/simclr_uci_P200 & 
-python hsearch_main.py --data ../data/ --cpu 0.2 --gpu 0.2 --max_concurrent 20 --experiment simclr_all/all_transformations/simclr/transf/uci/simclr_uci_P25 & 
-python hsearch_main.py --data ../data/ --cpu 0.2 --gpu 0.2 --max_concurrent 20 --experiment simclr_all/all_transformations/simclr/transf/uci/simclr_uci_P50 & 
-python hsearch_main.py --data ../data/ --cpu 0.2 --gpu 0.2 --max_concurrent 20 --experiment simclr_all/all_transformations/simclr/transf/uci/simclr_uci_P75
